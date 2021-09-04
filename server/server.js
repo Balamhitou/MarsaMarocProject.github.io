245024ce4ca952fc9f1dbf4373b325da1f311c19
Ghoufal Mohamed <ghoufalmohamed@gmail.com>
2021-09-04 13:31:55 +0100

    adding x-auth

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const port=process.env.PORT || 3000;
 var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-auth");
   next();
 });
 app.use(bodyParser.json());
