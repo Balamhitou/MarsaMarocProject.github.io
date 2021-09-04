fcbb8afa17005c728cc10a550dba665f904367e3
Ghoufal Mohamed <ghoufalmohamed@gmail.com>
2021-09-04 14:14:42 +0100

    adding middelwere

--- router/Sinistralités.js
+++ router/Sinistralités.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authenticate = require('../server/middelware/token_validation');
 
 const avaries = require('../server/models/sinistralités');
-router.post('/avaries',avaries.Sinistralités);
-router.post('/manquant',avaries.Manquants);
+router.post('/avaries',authenticate,avaries.Sinistralités);
+router.post('/manquant',authenticate,avaries.Manquants);
 module.exports = router;
--- router/statistics.js
+++ router/statistics.js
@@ -4,22 +4,22 @@ const staticsModel=require('../server/models/statistics');
 const authenticate = require('../server/middelware/token_validation');
 
 
-router.post('/GlobalImport',staticsModel.GlobalImport);//done
-router.post('/GlobalExport',staticsModel.GlobalExport);//done
-router.post('/ExportClientAnnuel',staticsModel.ExportClientAnnuel);//done
-router.post('/ImportClientAnnuel',staticsModel.ImportClientAnnuel);//done
-router.post('/ExportClient',staticsModel.ExportClient);//done
-router.post('/ImportClient',staticsModel.ImportClient);//done
-router.post('/sejour',staticsModel.delaiSejour);//done
-router.post('/Statisticsliberation',staticsModel.Liberation);//done
-router.post('/StatisticsAvaries',staticsModel.Avaries);//done
-router.post('/StatisticsManquants',staticsModel.Manquants);//done
-router.post('/StatisticSVA',staticsModel.Service);//done
-router.post('/tableau',staticsModel.Tableau);
-router.post('/DiagrammeRO',staticsModel.DiagrammeRO);
-router.get('/toutesVoitures',staticsModel.TouteVoitures);//done
-router.get('/TsVoituresparMarqe',staticsModel.VoituresParMarque);
-router.get('/ImportMoisAvant',staticsModel.ImportMoisAvant);//done
-router.get('/ExportMoisAvant',staticsModel.ExportMoisAvant);//done
-router.get('/SVAChaqueJour',staticsModel.SvaChaqueJour);//done
+router.post('/GlobalImport',authenticate,staticsModel.GlobalImport);//done
+router.post('/GlobalExport',authenticate,staticsModel.GlobalExport);//done
+router.post('/ExportClientAnnuel',authenticate,staticsModel.ExportClientAnnuel);//done
+router.post('/ImportClientAnnuel',authenticate,staticsModel.ImportClientAnnuel);//done
+router.post('/ExportClient',authenticate,staticsModel.ExportClient);//done
+router.post('/ImportClient',authenticate,staticsModel.ImportClient);//done
+router.post('/sejour',authenticate,staticsModel.delaiSejour);//done
+router.post('/Statisticsliberation',authenticate,staticsModel.Liberation);//done
+router.post('/StatisticsAvaries',authenticate,staticsModel.Avaries);//done
+router.post('/StatisticsManquants',authenticate,staticsModel.Manquants);//done
+router.post('/StatisticSVA',authenticate,staticsModel.Service);//done
+router.post('/tableau',authenticate,staticsModel.Tableau);
+router.post('/DiagrammeRO',authenticate,staticsModel.DiagrammeRO);
+router.get('/toutesVoitures',authenticate,staticsModel.TouteVoitures);//done
+router.get('/TsVoituresparMarqe',authenticate,staticsModel.VoituresParMarque);
+router.get('/ImportMoisAvant',authenticate,staticsModel.ImportMoisAvant);//done
+router.get('/ExportMoisAvant',authenticate,staticsModel.ExportMoisAvant);//done
+router.get('/SVAChaqueJour',authenticate,staticsModel.SvaChaqueJour);//done
 module.exports = router; 
