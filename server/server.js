35cb0e293fac67d826ce1ac4a47a5adecf48b7dc
Ghoufal Mohamed <ghoufalmohamed@gmail.com>
2021-09-09 15:32:44 +0100

    finaleApp

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const path = require('path');
 
 const port=process.env.PORT || 3000;
 var app = express();
+app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-auth");
